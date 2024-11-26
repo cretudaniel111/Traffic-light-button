dee4dca52134b3382c6830351d6c12e774e364b4
cretudaniel111 <146491245+cretudaniel111@users.noreply.github.com>
2024-11-26 12:01:57 +0200

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function changeColors() {
     const lightColors = ["btn-danger", "btn-warning", "btn-success"];
 
-    let button = document.getElementById("lights");
+    const button = document.getElementById("lights");
     
-    let currentColor = lightColors.findIndex(colors => button.classList.contains(lightColors));
+    const currentColor = lightColors.findIndex(colors => button.classList.contains(lightColors));
     button.classList.remove(lightColors[currentColor]);
     
-    let nextColor = (currentColor + 1) % lightColors.length;
+    const nextColor = (currentColor + 1) % lightColors.length;
     button.classList.add(lightColors[nextColor]);
 }
-setInterval(changeColors, 10000);
 
+setInterval(changeColors, 10000);
